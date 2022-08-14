c34675a9ce1ae79ebf663b6c301fc244588cb841
DangDinhTho <dangdinhtho1507@gmail.com>
2022-08-14 19:49:18 +0700

    port

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.use(router);
 //     app.use(morgan('dev'))
 // }
 
-const PORT = /*process.env.PORT || */ 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`Server is running on port ${PORT}`))
